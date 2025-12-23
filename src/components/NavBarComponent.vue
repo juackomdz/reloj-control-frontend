ee163b8c4e61ceae809ec9fd26b0161d4466e145
Joaquin Mendez <joaquin.enmt@gmail.com>
2025-12-23 13:27:46 -0300

    Fix: Mejora en el comportamiento de la navbar

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,8 +1,8 @@
 <template>
-    <v-navigation-drawer color="transparent" elevation="8" permanent :width="300">
+    <v-navigation-drawer color="transparent" elevation="8" :width="300" expand-on-hover rail>
         <v-icon icon="fa-solid fa-user" class="mt-4 ml-3"></v-icon>
         <v-divider class="mt-3"></v-divider>
-        <v-list-item link to="/" title="Home"></v-list-item>
-        <v-list-item link to="/" title="Link 2"></v-list-item>
+        <v-list-item prepend-icon="fa-solid fa-square-h" link to="/dashboard" title="Home"></v-list-item>
+        <v-list-item prepend-icon="fa-solid fa-chart-bar" link to="/dashboard/graph" title="Registros"></v-list-item>
     </v-navigation-drawer>
 </template>
