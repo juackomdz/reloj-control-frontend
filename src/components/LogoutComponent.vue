b3708e3dd31b98e3838e5617a54671893815e7b1
Joaquin Mendez <joaquin.enmt@gmail.com>
2025-12-24 10:50:41 -0300

    Feat: Boton logout transformado a componente

---
+++ src/components/LogoutComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useSesionStore } from "../stores/login"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+
+const useSesion = useSesionStore()
+const out = () =>{
+    useSesion.logout()
+    router.push("/")
+}
+</script>
+<template>
+    <v-col align="end">
+            <v-btn class="ml-auto" variant="tonal" @click="out">LOGOUT</v-btn>
+    </v-col>
+</template>
